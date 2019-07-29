325fbc94e2989f329b49efafa9c68c87b82ab9eb
tiagoPacheco <tiagoh.gpacheco@gmail.com>
2019-07-29 19:36:56 -0300

    New balanceService

---
+++ challenge-samsung/Services/IBalanceService.cs
@@ -0,0 +1,10 @@
+﻿using challenge_samsung.Models;
+using System.Collections.Generic;
+
+namespace challenge_samsung.Services
+{
+    public interface IBalanceService
+    {
+        List<Team> BalanceTeams();
+    }
+}
---
+++ challenge-samsung/Services/Impl/BalanceService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using challenge_samsung.Models;
+using System.Linq;
+
+namespace challenge_samsung.Services.Impl
+{
+    public class BalanceService : IBalanceService
+    {
+        private readonly GlobalStorage _globalStorage;
+
+        public BalanceService(GlobalStorage globalStorage)
+        {
+            _globalStorage = globalStorage;
+        }
+
+        public List<Team> BalanceTeams()
+        {
+            
+
+            return new List<Team>();
+        }
+    }
+}
